57f12536fea7d6827760b383f55cc4ef7e72e911
Insality <insality@gmail.com>
2016-06-19 18:09:17 +0500

    Updated config + added TODO Stubs

--- KJS/KJS.js
+++ KJS/KJS.js
@@ -1,55 +1,56 @@
-// TODO: Create the KJS Server here
-
 var express = require('express'),
-    path = require('path'),
-    app = express(),
-    fs = require('fs');
+	path = require('path'),
+	app = express(),
+	fs = require('fs');
 
+// Server Logic
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/projects'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-
+// Routes
+// TODO: Route every app with base template
 app.get('/', function (req, res) {
-    res.render('index', { project_list: project_list});
+	res.render('index', { project_list: project_list } );
 });
 
 
 
-// KJS logic: 
+// KJS Logic: 
 var project_list = [];
 collectData();
 
 
 function collectData() {
-    var project_path = path.join(__dirname, "/projects");
-    fs.readdir(project_path, function(err, items) {
-        if (err) throw err;
-        
-        // iterate over dirs:
-        items.forEach(function(item) {
-            var config_path = path.join(project_path, "/" + item + "/config.js");
-            fs.exists(config_path, function(isExist){
-                if (isExist) {
-                    fs.readFile(config_path, 'utf8', function (err, data) {
-                        if (err) throw err;
-                        var config = JSON.parse(data);
-                        
-                        config["img_path"] = "/" + item + "/icon.png";
-                        config["link"] = item;
-                        project_list.push(config);
-                    });
-                }
-            })
-        })
-    });
+	var project_path = path.join(__dirname, "/projects");
+	fs.readdir(project_path, function(err, items) {
+		if (err) throw err;
+
+		// iterate over dirs:
+		items.forEach(function(item) {
+			var config_path = path.join(project_path, "/" + item + "/config.js");
+
+			fs.exists(config_path, function(isExist){
+				if (isExist) {
+					fs.readFile(config_path, 'utf8', function (err, data) {
+						if (err) throw err;
+						var config = JSON.parse(data);
+
+						config["img_path"] = "/" + item + "/icon.png";
+						config["link"] = item;
+						project_list.push(config);
+					});
+				}
+			})
+		})
+	});
 }
 
-
+// TODO: Sort by create date?
 
 
 app.listen(80, function () {
-  console.log('Example app listening on port!');
+	console.log('KJS Server started');
 });
--- KJS/projects/TestProject/config.js
+++ KJS/projects/TestProject/config.js
@@ -1,5 +1,9 @@
 {
     "name": "Test project",
-    "description": "here is description",
-    "version": 1.0
+    "short_description": "here is description",
+    "full_description": "full description",
+    "create_date": "2016/06/19",
+    "author": "Insality, Kate",
+    "version": 1.0,
+    "size": "9 MB",
 }
